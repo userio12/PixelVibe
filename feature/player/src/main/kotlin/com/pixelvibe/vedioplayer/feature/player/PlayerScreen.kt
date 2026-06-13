144545aa8d2440a7ac893ff1faee76e6af558dfb
Userio12 <freeredeemcode1237.com>
2026-06-13 18:23:00 +0530

    fix missing LaunchedEffect import

--- feature/player/src/main/kotlin/com/pixelvibe/vedioplayer/feature/player/PlayerScreen.kt
+++ feature/player/src/main/kotlin/com/pixelvibe/vedioplayer/feature/player/PlayerScreen.kt
@@ -59,6 +59,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
